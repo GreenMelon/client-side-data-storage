83dddf4b390b6230f8fb03f5c93e0f2dd274fd9f
caigua <caigua@huanleguang.com>
2017-11-16 01:16:32 +0800

    indexedDB.open(db_name, version)

--- app/config/routes.js
+++ app/config/routes.js
@@ -5,18 +5,17 @@
 import NotFound from '../pages/404';
 import Index from '../pages/index';
 
-export default [
-    {
-        path: '/',
-        redirect: '/index'
-    }, {
-        path: '/index',
-        component: Index
-    },
+import IndexedDB from '../pages/indexed-database/index';
+import CreateDatabase from '../pages/indexed-database/create-database/001';
 
-    {
-        path: '*',
-        component: NotFound
-    }
+export default [
+
+    { path: '/', redirect: '/index' },
+    { path: '/index', component: Index },
+
+    { path: '/indexed-database', component: IndexedDB },
+    { path: '/indexed-database/create-database/001', component: CreateDatabase },
+
+    { path: '*', component: NotFound }
 
 ];
---
+++ app/pages/indexed-database/create-database/001.vue
@@ -0,0 +1,36 @@
+<template>
+    <main>
+        <h1>使用数据库</h1>
+    </main>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {}
+        },
+        methods: {
+            init() {
+                // 第一个参数是数据库名，第二个参数是版本号，一般从1开始
+                const openRequest = indexedDB.open('idb1', 1);
+
+                openRequest.onupgradeneeded = e => {
+                    console.log('running onupgradeneeded');
+                };
+
+                openRequest.onsuccess = e => {
+                    console.log('running success');
+                    let db  = e.target.result;
+                };
+
+                openRequest.onerror = e => {
+                    console.log('error');
+                    console.dir(e);
+                };
+            }
+        },
+        mounted() {
+            this.init();
+        }
+    }
+</script>
